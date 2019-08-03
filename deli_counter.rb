4bc329794945cdccf1587a95d6161d07a4973805
Marianne Pyon <mariannepyon@gmail.com>
2019-08-03 19:09:13 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,8 +1,13 @@
 # Write your code here.
-def line(customers)
-  katz_deli = [ ]
-  customers.each do |name|
-    puts "The line is currently empty."
+katz_deli = [ ]
+
+def line(x)
+  line_array = []
+    if x.length == 0
+      puts "The line is currently empty."
+    else
+      x.each.with_index(1) do |name, index|
+        line_array.push("#(index). #(name)")
   end
 end
 
