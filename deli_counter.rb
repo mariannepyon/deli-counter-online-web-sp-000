2faa8e158bd23664fcd79a361b940cdd68362d9b
Marianne Pyon <mariannepyon@gmail.com>
2019-08-03 18:52:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,17 @@
 # Write your code here.
+def line(customers)
+  katz_deli = [ ]
+  customers.each do 
+  else
+    puts "The line is currently empty."
+  end
+end
+
+
+def take_a_number(katz_deli, name)
+
+end
+
+def now_serving
+
+  end
