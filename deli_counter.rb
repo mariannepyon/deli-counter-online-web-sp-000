e2aaf603d6495f4bb94601b86e54685d57207a31
Marianne Pyon <mariannepyon@gmail.com>
2019-08-03 19:21:41 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -18,7 +18,7 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def now_serving
+def now_serving(array)
   if array.empty?
     puts "There is nobody waiting to be served!"
   else
