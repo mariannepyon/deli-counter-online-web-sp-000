8a4440b33a13bc53f9c6327048be2ed43df8f463
Marianne Pyon <mariannepyon@gmail.com>
2019-08-03 19:15:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,14 +7,14 @@ def line(x)
       puts "The line is currently empty."
     else
       x.each.with_index(1) do |name, index|
-        line_array.push("#(index). #(name)")
+        line_array.push("#{index}. #{name}")
     end
   end
 end
 
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
-  puts "Welcome, #(name). You are number #{katz_deli.length} in line."
+  puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
 def now_serving
